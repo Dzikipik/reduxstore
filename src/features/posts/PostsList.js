0ba1afe17f3deaa381783fe1cb374e5f4bc63a7e
Karola Glowacz <d3ikipik@gmail.com>
2023-01-26 18:48:21 +0100

    add ReactionButtons to posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,20 +3,24 @@ import { selectAllPosts } from "./postsSlice";
 
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const orderedPosts = posts.slice().sort((a,b)=> b.date.localeCompare(a.date));
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
 
   const renderedPosts = orderedPosts.map((post) => (
     <article className="post" key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
-        <PostAuthor userId={post.userId}/>
-        <TimeAgo timestamp={post.date}/>
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   ));
   return (
